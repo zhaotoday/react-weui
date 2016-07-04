14849649659ab45acd85726a908811fce76baa08
zhaotoday <729234283@qq.com>
2016-07-04 23:43:45 +0800

    update

--- src/app/help/index.js
+++ src/app/help/index.js
@@ -1,9 +1,85 @@
 import React from 'react'
+import Panel from 'components/weui/panel'
+import MediaBox from 'components/weui/mediaBox'
 
 module.exports = class extends React.Component {
   render() {
-    return <div>
-      我来帮忙
-    </div>
+    return <Panel>
+      <Panel.Header>
+        我来帮忙
+      </Panel.Header>
+      <Panel.Body>
+        <MediaBox type="text">
+          <MediaBox.Title>八堂带饭</MediaBox.Title>
+          <MediaBox.Description>
+            哪位顺路的兄弟，帮我到八堂打下饭，米饭一盒+回锅肉一份+清汤一份，速度要快，快饿死了，快快快快！谢谢！
+          </MediaBox.Description>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>起点：福建农林大学第八食堂<br/>终点：南区 4 号楼 235 宿舍</MediaBox.InfoMeta>
+          </MediaBox.Info>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>悬赏：2 元</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>发布：07-04 12:00</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>结束：07-04 13:00</MediaBox.InfoMeta>
+          </MediaBox.Info>
+        </MediaBox>
+        <MediaBox type="text">
+          <MediaBox.Title>八堂带饭</MediaBox.Title>
+          <MediaBox.Description>
+            哪位顺路的兄弟，帮我到八堂打下饭，米饭一盒+回锅肉一份+清汤一份，速度要快，快饿死了，快快快快！谢谢！
+          </MediaBox.Description>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>起点：福建农林大学第八食堂<br/>终点：南区 4 号楼 235 宿舍</MediaBox.InfoMeta>
+          </MediaBox.Info>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>悬赏：2 元</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>发布：07-04 12:00</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>结束：07-04 13:00</MediaBox.InfoMeta>
+          </MediaBox.Info>
+        </MediaBox>
+        <MediaBox type="text">
+          <MediaBox.Title>八堂带饭</MediaBox.Title>
+          <MediaBox.Description>
+            哪位顺路的兄弟，帮我到八堂打下饭，米饭一盒+回锅肉一份+清汤一份，速度要快，快饿死了，快快快快！谢谢！
+          </MediaBox.Description>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>起点：福建农林大学第八食堂<br/>终点：南区 4 号楼 235 宿舍</MediaBox.InfoMeta>
+          </MediaBox.Info>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>悬赏：2 元</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>发布：07-04 12:00</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>结束：07-04 13:00</MediaBox.InfoMeta>
+          </MediaBox.Info>
+        </MediaBox>
+        <MediaBox type="text">
+          <MediaBox.Title>八堂带饭</MediaBox.Title>
+          <MediaBox.Description>
+            哪位顺路的兄弟，帮我到八堂打下饭，米饭一盒+回锅肉一份+清汤一份，速度要快，快饿死了，快快快快！谢谢！
+          </MediaBox.Description>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>起点：福建农林大学第八食堂<br/>终点：南区 4 号楼 235 宿舍</MediaBox.InfoMeta>
+          </MediaBox.Info>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>悬赏：2 元</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>发布：07-04 12:00</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>结束：07-04 13:00</MediaBox.InfoMeta>
+          </MediaBox.Info>
+        </MediaBox>
+        <MediaBox type="text">
+          <MediaBox.Title>八堂带饭</MediaBox.Title>
+          <MediaBox.Description>
+            哪位顺路的兄弟，帮我到八堂打下饭，米饭一盒+回锅肉一份+清汤一份，速度要快，快饿死了，快快快快！谢谢！
+          </MediaBox.Description>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>起点：福建农林大学第八食堂<br/>终点：南区 4 号楼 235 宿舍</MediaBox.InfoMeta>
+          </MediaBox.Info>
+          <MediaBox.Info>
+            <MediaBox.InfoMeta>悬赏：2 元</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>发布：07-04 12:00</MediaBox.InfoMeta>
+            <MediaBox.InfoMeta>结束：07-04 13:00</MediaBox.InfoMeta>
+          </MediaBox.Info>
+        </MediaBox>
+      </Panel.Body>
+    </Panel>
   }
 }
